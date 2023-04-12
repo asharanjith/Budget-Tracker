ec6e8852c0a0bbabf04c1fbbe92f6eec7fdb64d8
asharanjith <ashasvijayan@gmail.com>
2023-04-12 11:22:42 +0400

    Add styles to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'groups#index'
+  root "splash#index"
 
   resources :users
+
   resources :groups do
     resources :expenses
   end
