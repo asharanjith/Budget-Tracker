f3f383d46758469137870ad0b99cdfc14d29846c
asharanjith <ashasvijayan@gmail.com>
2023-04-12 12:52:12 +0400

    Finish styling and fix errors

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -11,7 +11,7 @@ class ExpensesController < ApplicationController
 
   def create
     @expense = Expense.new(expense_params)
-    @groups = params[:groups] || params[:groups][:id]
+    @groups = params[:groups] || []
     @groups.each do |id|
       group = Group.find(id) unless id == ''
       @expense.groups << group unless group.nil?
@@ -19,10 +19,13 @@ class ExpensesController < ApplicationController
     @expense.author = current_user
     respond_to do |format|
       if @expense.save
-        format.html { redirect_to group_expenses_path, notice: 'Expense was successfully created.' }
+        format.html { redirect_to group_expenses_path, notice: 'Expenditure successfully created.' }
         format.json { render :show, status: :created, location: @expense }
       else
-        format.html { render :new, status: :unprocessable_entity, notice: 'Expense was not added.' }
+        format.html do
+          redirect_to group_expenses_path, status: :unprocessable_entity,
+                                           notice: 'Expenditure not added. All fields mandatory'
+        end
         format.json { render json: @expense.errors, status: :unprocessable_entity }
       end
     end
